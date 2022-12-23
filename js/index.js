4e47088bcb40f2b55e70771f81c88c5832991317
EstherAma <estherdlz@hotmail.com>
2022-12-23 18:17:09 +0100

    header and footer finished

--- js/index.js
+++ js/index.js
@@ -13,18 +13,16 @@ fetchBooksJson().then(books =>{
     {
         const booksSection = document.getElementById("bookSection");
         
-        //console.log(books.misLibros[index].titulo);
-        
-        let id = books.misLibros[index].id;
-        let img = books.misLibros[index].caratula;
-        let title = books.misLibros[index].titulo;
-        let description = books.misLibros[index].descripcion;
-        let genre = books.misLibros[index].genero;
-        let author = books.misLibros[index].autor;
-        let yearPublication=books.misLibros[index].añoDePublicacion;
+            let id = books.misLibros[index].id;
+            let img = books.misLibros[index].caratula;
+            let title = books.misLibros[index].titulo;
+            let description = books.misLibros[index].descripcion;
+            let genre = books.misLibros[index].genero;
+            let author = books.misLibros[index].autor;
+            let yearPublication=books.misLibros[index].añoDePublicacion;
 
         booksSection.innerHTML += `
-            <div class="card" style="width: 21rem;">
+            <div class="card" style="width: 22rem;">
                 <img src="${img}" class="card-img-top" alt="Portada del libro">
                 <div class="card-body">
                     <h4 class="card-title cardTitle">${id}. ${title}</h4>
@@ -40,14 +38,11 @@ fetchBooksJson().then(books =>{
                     </details>
 
                     <div class="buyIt">
-                    <a href="https://www.casadellibro.com/libro-la-sombra-de-la-sirena-serie-fjallbacka-6/9788415140924/2109337?q=serie+fjallbacka" class="btn btn-outline-success">Comprar</a>
+                    <a href="https://www.casadellibro.com/libro-la-sombra-de-la-sirena-serie-fjallbacka-6/9788415140924/2109337?q=serie+fjallbacka" class="btn btn-outline-secondary">Comprar</a>
                 </div>    
  
             </div>
         `
-
-
-
     };
 })
 
